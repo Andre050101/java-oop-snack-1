bc723ab7f949ce1491292858bc5179b46df5130c
Andre050101 <agiancristiano90@gmail.com>
2025-03-18 17:41:29 +0100

    snack 3 done

--- src/App.java
+++ src/App.java
@@ -1,16 +1,9 @@
-import org.lessons.java.snack1.Studente;
-
+import org.lessons.java.snack1.*;
 import java.math.BigDecimal;
 
-import org.lessons.java.snack1.ContoBancario;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        ContoBancario contoDiProva = new ContoBancario(0);
-        System.out.println(contoDiProva.getNumeroConto());
-        System.out.println(contoDiProva.getSaldo());
-        contoDiProva.deposit(new BigDecimal(5000));
-        System.out.println(contoDiProva.getSaldo());
-        contoDiProva.withdrawal(new BigDecimal(2000));
-        System.out.println(contoDiProva.getSaldo());
+        
     }
 }
--- src/org/lessons/java/snack1/RegistroStudenti.java
+++ src/org/lessons/java/snack1/RegistroStudenti.java
@@ -1,5 +1,49 @@
 package org.lessons.java.snack1;
-
+import org.lessons.java.snack1.Studente;
+import java.util.Arrays;
+/*Crea una classe RegistroStudenti che rappresenti un registro di studenti.
+  Utilizza un array privato per memorizzare oggetti Studente. 
+  Implementa un costruttore senza parametri per inizializzare il registro 
+  vuoto e un metodo pubblico per aggiungere studenti al registro. 
+  Aggiungi un metodo che stampi la lista degli studenti.*/
 public class RegistroStudenti {
+    //Variabili di istanza
+    private Studente[] arrayStudenti;
+    private int nStudenti;
 
+    //Costruttore
+    public RegistroStudenti(int dimensioneMassima){
+        this.arrayStudenti= new Studente[dimensioneMassima];
+        this.nStudenti = 0;
+    }
+
+    //Metodo per ridimensionare l'array in caso fosse pieno:
+    private void ridimensionaArray(){
+        int nuovaDim = arrayStudenti.length * 2;
+        arrayStudenti = Arrays.copyOf(arrayStudenti, nuovaDim);
+        System.out.println("Registro pieno! Espansione a " + nuovaDim + " posti.");
+
+    }
+    //Metodo per aggiungere studente
+    public void aggiungiStudente(Studente studente){
+        if(this.nStudenti < arrayStudenti.length){
+            arrayStudenti[nStudenti] = studente;
+            nStudenti++;
+        }
+        else{
+            ridimensionaArray();
+        }
+    }
+
+    public void stampaStudenti(){
+        if(nStudenti == 0){
+            System.out.println("Registro vuoto");
+        }
+        else{
+            System.out.println("Lista degli studenti:");
+            for(int i = 0; i < nStudenti; i++){
+                System.out.println(arrayStudenti[i].concatenatedValues());
+            }
+        }
+    }
 }
