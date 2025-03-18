35455e78dce1822d34bdc82adcd83e1bb874563e
Andre050101 <agiancristiano90@gmail.com>
2025-03-18 15:54:17 +0100

    snack 1 and 2 done

--- src/App.java
+++ src/App.java
@@ -1,8 +1,16 @@
 import org.lessons.java.snack1.Studente;
+
+import java.math.BigDecimal;
+
+import org.lessons.java.snack1.ContoBancario;
 public class App {
     public static void main(String[] args) throws Exception {
-        Studente andrea = new Studente("Andrea", "Giancristiano", 24);
-
-        System.out.println(andrea.concatenatedValues());
+        ContoBancario contoDiProva = new ContoBancario(0);
+        System.out.println(contoDiProva.getNumeroConto());
+        System.out.println(contoDiProva.getSaldo());
+        contoDiProva.deposit(new BigDecimal(5000));
+        System.out.println(contoDiProva.getSaldo());
+        contoDiProva.withdrawal(new BigDecimal(2000));
+        System.out.println(contoDiProva.getSaldo());
     }
 }
--- src/org/lessons/java/snack1/ContoBancario.java
+++ src/org/lessons/java/snack1/ContoBancario.java
@@ -1,5 +1,46 @@
 package org.lessons.java.snack1;
 
-public class ContoBancario {
+import java.math.BigDecimal;
 
+/*Crea una classe ContoBancario con attributi per numero di conto e saldo. 
+  Implementa un costruttore che accetta il numero di conto e inizializza il saldo a zero. 
+  Aggiungi metodi pubblici per depositare denaro sul conto, prelevare denaro dal conto e ottenere il saldo corrente.*/
+public class ContoBancario {
+    private int numeroConto;
+    private BigDecimal saldo;
+
+    public ContoBancario(int numeroConto){
+        this.numeroConto = numeroConto;
+        this.saldo = new BigDecimal(0);
+    }
+
+    //Getter (setter non necessari perche non ha senso cambiare numero di conto o saldo(quest'ultimo manualmente non ha senso))
+    public int getNumeroConto(){
+        return this.numeroConto;
+    }
+
+    public BigDecimal getSaldo(){
+        return this.saldo;
+    }
+
+    //Other methods
+    public void deposit(BigDecimal soldiDaDepositare){
+        if(soldiDaDepositare.compareTo(new BigDecimal(0)) == 1){
+            this.saldo = this.saldo.add(soldiDaDepositare);
+            System.out.println("Deposito di "+soldiDaDepositare+"€ effettuato. Saldo attuale: "+this.saldo+"€");
+        }
+        else{
+            System.out.println("Errore in fase di deposito");
+        }
+    }
+
+    public void withdrawal(BigDecimal soldiDaPrelevare){
+        if(soldiDaPrelevare.compareTo(this.saldo) == 1){
+            System.out.println("Disponibilità insufficiente");
+        }
+        else{
+            this.saldo = this.saldo.subtract(soldiDaPrelevare);
+            System.out.println("Prelievo di "+soldiDaPrelevare+"€ effettuato! Saldo rimanente: "+this.saldo+"€");
+        }
+    }
 }
